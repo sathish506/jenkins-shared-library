d95c73866a94b951e90aca6c433ea3d1eea38a19
Sathish <sathishu269@gmail.com>
2023-10-16 12:50:12 +0530

    Auto Push

---
+++ vars/mvn.groovy
@@ -0,0 +1,30 @@
+def lintchecks() {
+   
+        sh "echo starting link checks for ${COMPONENT}"
+        sh "echo mvn checkstyle:check || true"
+        sh "echo link checks completed for ${COMPONENT}"
+    }
+
+def call() {
+  pipeline {
+    agent any 
+    stages {
+        stage('Lint Checks') {
+            steps {
+             script {
+                lintchecks()
+             }
+                
+            }
+        }
+
+        stage('Generating artifacts') {
+            steps {
+                sh "echo Generating artifacts"
+                sh "npm install"
+
+                }
+            }
+        }
+    }
+}
